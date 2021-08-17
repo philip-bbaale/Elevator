0eaa525a3d4063a940610d129b6f5d9d00c75c94
Philip Namugera Bbaale <namugera.philip@gmail.com>
2021-08-17 15:27:22 +0300

    correcting elevator current floor variable

--- main.py
+++ main.py
@@ -5,27 +5,34 @@ class Operator():
     def __init__(self,inputs):
         self.inputs = inputs
         self.elevator = Elevator(12)
-        self.car_current_floor = self.elevator.car_current_floor
 
     def operate(self):
         locations = []
         for pair in range(len(self.inputs)):
 
-            # if self.inputs[pair][0] == "up" or "down":
-            #     self.elevator.call_elevator(self.inputs[pair][1])
-            #     locations.append(self.inputs[pair][1])
-            # elif self.inputs[pair][0] == "go":
-            #     self.elevator.go_floor(self.inputs[pair][1])
-            #     locations.append(self.inputs[pair][1])
-
             if self.inputs[pair][0] == "up":
-                self.elevator.car_call_up(self.inputs[pair][1])
+
+                if self.inputs[pair][1] > self.elevator.car_current_floor:
+                    self.elevator.car_call_up(self.inputs[pair][1])
+
+                elif self.inputs[pair][1] <= self.elevator.car_current_floor:
+                    self.elevator.car_call_down(self.inputs[pair][1])
 
             if self.inputs[pair][0] == "down":
-                self.elevator.car_call_down(self.inputs[pair][1])
+
+                if self.inputs[pair][1] > self.elevator.car_current_floor:
+                    self.elevator.car_call_up(self.inputs[pair][1])
+
+                elif self.inputs[pair][1] <= self.elevator.car_current_floor:
+                    self.elevator.car_call_down(self.inputs[pair][1])
 
             if self.inputs[pair][0] == "go":
-                pass
+                
+                if self.inputs[pair][1] > self.elevator.car_current_floor:
+                    self.elevator.car_go_up(self.inputs[pair][1])
+
+                elif self.inputs[pair][1] <= self.elevator.car_current_floor:
+                    self.elevator.car_go_down(self.inputs[pair][1])
                 
         print(locations)
 
