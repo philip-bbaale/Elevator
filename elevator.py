13b3f91f6934ae2d189bf16d3663c0a283b3f4dd
Philip Namugera Bbaale <namugera.philip@gmail.com>
2021-09-21 21:17:39 +0300

    adding docstrings

--- elevator.py
+++ elevator.py
@@ -1,3 +1,4 @@
+#Elevator class/ Instance of the elvator
 class Elevator():
   def __init__(self, num_floors):
     self.total_floors = num_floors
@@ -5,25 +6,31 @@ class Elevator():
     self.max_floor = 12
     self.min_floor = 0
 
+  #Function to get the car's current floor
   def get_car_current_floor(self):
     return self.car_current_floor
 
+  #Function to print the car's current floor
   def is_at(self):
     print("Currently on {}'nd floor".format(self.car_current_floor))
 
+  #Function to make car move up
   def move_up(self):
     self.is_at()
     self.car_current_floor = self.car_current_floor + 1
     print("Moving up {}'nd floor".format(self.car_current_floor))
 
+  #Function to make car move down
   def move_down(self):
     self.is_at()
     self.car_current_floor = self.car_current_floor -1
     print("Moving down {}'nd floor".format(self.car_current_floor))
 
+  #Function to stop car
   def stop(self):
     print("Opening Doors \n Closing Doors",self.car_current_floor)
 
+  #Function to call car on a higher floor
   def car_call_up(self, user_call_floor):
     self.user_call_floor = user_call_floor
     while self.car_current_floor != self.user_call_floor and self.car_current_floor < self.max_floor:
@@ -31,6 +38,7 @@ class Elevator():
     else:
       self.stop()
 
+  #Function to call car to lower floor
   def car_call_down(self, user_call_floor):
     self.user_call_floor = user_call_floor
     while self.car_current_floor != self.user_call_floor and self.car_current_floor > self.min_floor:
@@ -38,6 +46,7 @@ class Elevator():
     else:
       self.stop()
     
+  #Function to tell car to go to higher floor
   def car_go_up(self, user_go_floor):
     self.user_go_floor = user_go_floor
     while self.car_current_floor != self.user_go_floor:
@@ -45,6 +54,7 @@ class Elevator():
     else:
       self.stop()
 
+  #Function to tell car to go to lower floor
   def car_go_down(self, user_go_floor):
     self.user_go_floor = user_go_floor
     while self.car_current_floor != self.user_go_floor:
