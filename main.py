b2d23029730e36d6390270871bfdbb04b1b148eb
Philip Namugera Bbaale <namugera.philip@gmail.com>
2021-09-21 21:28:02 +0300

    adding docstrings

--- main.py
+++ main.py
@@ -1,43 +1,55 @@
+#Importin Elevator class to access its functions.
 from elevator import Elevator
 
+#Operator class to make use of the Elevator functions.
 class Operator():
 
+    #Initializing user inputs and number of floors on building.
     def __init__(self,inputs):
         self.inputs = inputs
         self.elevator = Elevator(12)
 
-    
+    #Function to run the elevator
     def operate(self):
         locations = []
         
         for pair in range(len(self.inputs)):
-
+            
+            #Run if call operator is going up
             if self.inputs[pair][0] == "up":
 
+                #Run if Call operator is on a higher floor than the car's current floor.
                 if self.inputs[pair][1] > self.elevator.car_current_floor:
                     self.elevator.car_call_up(self.inputs[pair][1])
                     locations.append(self.inputs[pair][1])
 
+                #Run if the call operator is a lower floor than the car's current floor.
                 elif self.inputs[pair][1] <= self.elevator.car_current_floor:
                     self.elevator.car_call_down(self.inputs[pair][1])
                     locations.append(self.inputs[pair][1])
 
+            #Run if call operator is going down
             elif self.inputs[pair][0] == "down":
 
+                #Run if call operator is on a higher floor than the car's current floor.
                 if self.inputs[pair][1] > self.elevator.car_current_floor:
                     self.elevator.car_call_up(self.inputs[pair][1])
                     locations.append(self.inputs[pair][1])
 
+                #Run if call operator is on a lower or on the same floor as the car.
                 elif self.inputs[pair][1] <= self.elevator.car_current_floor:
                     self.elevator.car_call_down(self.inputs[pair][1])
                     locations.append(self.inputs[pair][1])
 
+            #Run if call operator is inside the car and going to a floor
             elif self.inputs[pair][0] == "go":
                 
+                #Run if call operator is going to a higher floor
                 if self.inputs[pair][1] > self.elevator.car_current_floor:
                     self.elevator.car_go_up(self.inputs[pair][1])
                     locations.append(self.inputs[pair][1])
-
+                    
+                #Run if call operator is on a lower on same floor as car
                 elif self.inputs[pair][1] <= self.elevator.car_current_floor:
                     self.elevator.car_go_down(self.inputs[pair][1])
                     locations.append(self.inputs[pair][1])
