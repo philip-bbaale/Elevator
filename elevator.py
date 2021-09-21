7a34163ef173931c9976b1e954dbf3d7d3f139d2
Philip Namugera Bbaale <namugera.philip@gmail.com>
2021-09-21 21:36:26 +0300

    adding docstrings

--- elevator.py
+++ elevator.py
@@ -1,10 +1,10 @@
-#Elevator class/ Instance of the elvator
+#Elevator class/ Instance of the elvator.
 class Elevator():
   def __init__(self, num_floors):
     self.total_floors = num_floors
     self.elevatator_current_floor = 0
   
-  #Function to get the car's current floor
+  #Function to get the car's current floor.
   def elevator_current_floor(self):
     return self.elevatator_current_floor
   
@@ -12,23 +12,23 @@ class Elevator():
   def is_at(self):
     print("Currently on {}'nd floor".format(self.elevatator_current_floor))
   
-  #Function to make car move up
+  #Function to make car move up.
   def move_up(self):
     self.is_at()
     self.elevatator_current_floor = self.elevatator_current_floor + 1
     print("Moving up {}'nd floor".format(self.elevatator_current_floor))
 
-  #Function to make car move down
+  #Function to make car move down.
   def move_down(self):
     self.is_at()
     self.elevatator_current_floor = self.elevatator_current_floor -1
     print("Moving down {}'nd floor".format(self.elevatator_current_floor))
 
-  #Function to stop car
+  #Function to stop car.
   def stop(self):
     print("Opening Doors \n Closing Doors")
   
-  #Function to call car
+  #Function to call car.
   def call_elevator(self, user_call_floor):
     self.user_call_floor = user_call_floor
 
@@ -44,7 +44,7 @@ class Elevator():
       else:
         self.stop()
 
-  #Function to tell car to go to floor
+  #Function to tell car to go to floor.
   def go_floor(self, user_go_floor):
     self.user_go_floor = user_go_floor
 
