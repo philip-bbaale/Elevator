dd56453d0a9c2b86c5385768bb29684d32dcb99a
Philip Namugera Bbaale <namugera.philip@gmail.com>
2021-10-15 09:42:31 +0300

    correct elevator spelling

--- tests/functional/test_elevator_is_efficient.py
+++ tests/functional/test_elevator_is_efficient.py
@@ -9,7 +9,7 @@ def test_case_1():
     assert Op.run(b) == [3, 12, 7, 7]
 
 def test_case_2():
-    assert Op.elevator.elevatator_current_floor() == 7
+    assert Op.elevator.elevator_current_floor() == 7
 
     a = [("up",3),("go",12),("up",7),("down",7),("go",7),("down",11),("down",12)]
     assert Op.run(a) == [3, 12, 7, 7, 7, 11, 12]
